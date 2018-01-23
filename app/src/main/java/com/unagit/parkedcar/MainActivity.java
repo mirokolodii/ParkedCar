d97e090ac8a610bd6718641296e480967bae0b5a
Myroslav Kolodii <mkolodii@gmail.com>
2018-01-23 16:14:41 +0100

    Fix double action bar issue and change app name in strings.xml

--- app/src/main/java/com/unagit/parkedcar/MainActivity.java
+++ app/src/main/java/com/unagit/parkedcar/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Not sure if I need this, works just fine without this code
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        */
 
         setupViewPagerAndTabLayout();
 
