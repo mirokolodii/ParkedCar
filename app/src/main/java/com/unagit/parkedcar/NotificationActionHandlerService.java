d74b304bd0580ce40445794ddbf26680c7008ece
Myroslav Kolodii <mkolodii@gmail.com>
2018-02-13 11:09:58 +0100

    NotificationActionHandlerService: put code, related to 'show_on_map' action, into separate method.

--- app/src/main/java/com/unagit/parkedcar/NotificationActionHandlerService.java
+++ app/src/main/java/com/unagit/parkedcar/NotificationActionHandlerService.java
@@ -27,29 +27,9 @@ public class NotificationActionHandlerService extends IntentService {
         // Show parking location in Google Maps
         if (action.equals(Constants.Notifications.ACTION_SHOW_ON_MAP)) {
             Log.i(LOG_TAG, "Show_on_map action");
+            // Show location on google maps
+            showLocationOnMaps();
 
-            // Create Google Maps query URL
-            MyDefaultPreferenceManager myPreferenceManager = new MyDefaultPreferenceManager(getApplicationContext());
-            String uri = Constants.GoogleMaps.GOOGLE_MAPS_QUERY_URL;
-            uri += myPreferenceManager.getLatitude() + "," + myPreferenceManager.getLongitude();
-            // Initiate new intent
-            Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            // Open intent in Google Maps
-            mapsIntent.setPackage("com.google.android.apps.maps");
-            try {
-                startActivity(mapsIntent);
-            }
-            // If Google Maps app is not installed, try with other apps that can handle this intent
-            catch (ActivityNotFoundException ex) {
-                try {
-                    Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                    startActivity(unrestrictedIntent);
-                }
-                // No apps are available on a device to handle the intent
-                catch (ActivityNotFoundException innerEx) {
-                    Toast.makeText(this, "Install Google Maps first.", Toast.LENGTH_LONG).show();
-                }
-            }
 
         } else if (action.equals(Constants.Notifications.ACTION_DIRECTIONS)) {
             // TODO: Show directions from current location to parking location in Google Maps
@@ -64,6 +44,31 @@ public class NotificationActionHandlerService extends IntentService {
             throw new IllegalArgumentException("Unsupported action: " + action);
         }
         collapseNotificationBar();
+    }
+
+    private void showLocationOnMaps() {
+        // Create Google Maps query URL
+        MyDefaultPreferenceManager myPreferenceManager = new MyDefaultPreferenceManager(getApplicationContext());
+        String uri = Constants.GoogleMaps.GOOGLE_MAPS_QUERY_URL;
+        uri += myPreferenceManager.getLatitude() + "," + myPreferenceManager.getLongitude();
+        // Initiate new intent
+        Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        // Open intent in Google Maps
+        mapsIntent.setPackage("com.google.android.apps.maps");
+        try {
+            startActivity(mapsIntent);
+        }
+        // If Google Maps app is not installed, try with other apps that can handle this intent
+        catch (ActivityNotFoundException ex) {
+            try {
+                Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(unrestrictedIntent);
+            }
+            // No apps are available on a device to handle the intent
+            catch (ActivityNotFoundException innerEx) {
+                Toast.makeText(this, "Install Google Maps first.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public void dismissNotification() {
