dbbe8f6d830776af64ba99bb9b268779613638bb
Myroslav Kolodii <mkolodii@gmail.com>
2019-01-18 11:16:14 +0100

    Implement ParkView

--- app/src/main/java/com/unagit/parkedcar/views/ParkView.java
+++ app/src/main/java/com/unagit/parkedcar/views/ParkView.java
@@ -2,20 +2,27 @@ package com.unagit.parkedcar.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.LinearLayout;
+
+import com.unagit.parkedcar.R;
 
 import androidx.annotation.Nullable;
 
-public class ParkView extends View {
+public class ParkView extends LinearLayout {
     public ParkView(Context context) {
         super(context);
     }
 
     public ParkView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initView();
     }
 
     public ParkView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    private void initView() {
+        inflate(getContext(), R.layout.park_view, this);
     }
 }
