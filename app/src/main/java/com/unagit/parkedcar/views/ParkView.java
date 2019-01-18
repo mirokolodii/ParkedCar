de32f0e04635bc8e83acb9f19789a6c35abd149a
Myroslav Kolodii <mkolodii@gmail.com>
2019-01-18 10:46:09 +0100

    Add custom ParkView

---
+++ app/src/main/java/com/unagit/parkedcar/views/ParkView.java
@@ -0,0 +1,21 @@
+package com.unagit.parkedcar.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ParkView extends View {
+    public ParkView(Context context) {
+        super(context);
+    }
+
+    public ParkView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ParkView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
