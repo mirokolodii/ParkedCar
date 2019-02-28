b17b0d0903da83cabd51ae78dd376d3a83840b05
Myroslav Kolodii <mkolodii@gmail.com>
2019-02-28 13:57:39 +0100

    Update ParkView

--- app/src/main/java/com/unagit/parkedcar/views/ParkView.java
+++ app/src/main/java/com/unagit/parkedcar/views/ParkView.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -16,9 +15,18 @@ import android.widget.LinearLayout;
 
 import com.unagit.parkedcar.R;
 
+import java.util.ArrayList;
+
 import androidx.annotation.Nullable;
 
 public class ParkView extends LinearLayout {
+
+    private static int CONTAINER_WIDTH;
+    private static int CIRCLE_WIDTH;
+    private static int DIST_BETWEEN_CIRCLES;
+    private static int CIRCLES_COUNT = 3;
+    private static double[] CIRCLES_POS = new double[CIRCLES_COUNT];
+
     public ParkView(Context context) {
         super(context);
     }
@@ -34,58 +42,90 @@ public class ParkView extends LinearLayout {
 
     private void initView() {
         inflate(getContext(), R.layout.park_view, this);
-        FrameLayout circlesContainer = findViewById(R.id.circles_container);
-        CircleView circleView = new CircleView(getContext());
-        circlesContainer.addView(circleView);
+        FrameLayout container = findViewById(R.id.circles_container);
+        ArrayList<CircleView> circles = getCircles(1);//CIRCLES_COUNT);
+        for(CircleView circle : circles) {
+            container.addView(circle);
+        }
+        setDimens(container);
         Button btn = findViewById(R.id.park_car);
-        btn.setOnClickListener(v -> {
-            animate(circlesContainer, circleView);
-        });
-//        animate(circlesContainer, circleView);
+        btn.setOnClickListener(v -> startAnimation(circles));
+        Log.e("anim", "Number of circles: " + circles.size());
+
+    }
+
+    private ArrayList<CircleView> getCircles(Integer count) {
+        ArrayList<CircleView> circles = new ArrayList<>();
+        while (count > 0) {
+            circles.add(new CircleView(getContext()));
+            count--;
+        }
+        return circles;
+    }
+
+    private void setDimens(FrameLayout container) {
+        CONTAINER_WIDTH = container.getWidth();
+        Log.e("anim", "Container width: " + container.getWidth());
+        CIRCLE_WIDTH = container.getHeight();
+        DIST_BETWEEN_CIRCLES = 10;
+        CIRCLES_POS[0] = CONTAINER_WIDTH / 2.0 + CIRCLE_WIDTH / 2.0 + DIST_BETWEEN_CIRCLES;
+        CIRCLES_POS[1] = CONTAINER_WIDTH / 2.0 - CIRCLE_WIDTH / 2.0;
+        CIRCLES_POS[2] = CONTAINER_WIDTH / 2.0 + CIRCLE_WIDTH / 2.0 + DIST_BETWEEN_CIRCLES;
     }
 
     // https://www.raywenderlich.com/350-android-animation-tutorial-with-kotlin
-    private void animate(FrameLayout circlesContainer, CircleView circleView) {
-        ValueAnimator positionAnimator = ValueAnimator.ofFloat(0f, circlesContainer.getWidth() / 2f);
-        positionAnimator.addUpdateListener(animation -> {
-            Float value = (Float) animation.getAnimatedValue();
-            circleView.setTranslationX(value);
-        });
+    private void startAnimation(ArrayList<CircleView> circles) {
 
-        positionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-
-        ObjectAnimator fadeAnimator = ObjectAnimator.ofFloat(circleView, "alpha", 0f, 1f);
+//        ValueAnimator positionAnimator = ValueAnimator.ofFloat(0f, circlesContainer.getWidth() / 2f);
+//        positionAnimator.addUpdateListener(animation -> {
+//            Float value = (Float) animation.getAnimatedValue();
+//            circleView.setTranslationX(value);
+//        });
+//
+//        positionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+//
+//        ObjectAnimator fadeAnimator = ObjectAnimator.ofFloat(circleView, "alpha", 0f, 1f);
 
         AnimatorSet set = new AnimatorSet();
 
-        set.play(positionAnimator).with(fadeAnimator);
+//        set.play(positionAnimator).with(fadeAnimator);
+        set.play(getPositionAnimator(circles.get(0), CONTAINER_WIDTH));//(float) CIRCLES_POS[0]));
+        Log.e("anim", "CONTAINER WIDTH: " + CONTAINER_WIDTH);
         set.setDuration(1000);
 
-        set.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                Log.e("anim", "started");
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                Log.e("anim", "ended");
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
+//        set.addListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//                Log.e("anim", "started");
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                Log.e("anim", "ended");
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
 
         set.start();
     }
 
-
+    private ValueAnimator getPositionAnimator(View view, float endPos) {
+        ValueAnimator positionAnimator = ValueAnimator.ofFloat(0f, endPos);
+        positionAnimator.addUpdateListener(animation -> {
+            Float value = (Float) animation.getAnimatedValue();
+            view.setTranslationX(value);
+        });
+        positionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        return positionAnimator;
+    }
 }
